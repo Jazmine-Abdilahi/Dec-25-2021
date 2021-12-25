60274848cbf45e2c67f13017472d1454635b00c7
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-25 12:07:00 -0600

    Add files via upload

---
+++ Dec25.js
@@ -0,0 +1,12 @@
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100); {
+    console.log('Dolphins Win the trophy 🏆')
+} else if (scoreKoalas > scoreDolphins && scoreKoala >= 100); {
+    console.log('Koalas win the trophy 🏆');
+} else if (scoreKoalas === scoreDolphins && scoreKoala >= 100); {
+    console.log('both win the trophy🏆');
+} else {
+    console.log('nobody wins the trophy 😭 ');
+}
