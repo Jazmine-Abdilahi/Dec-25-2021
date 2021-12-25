7f0eea7e2e15019888c8d11bcd9c176c3305f55c
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-25 12:46:24 -0600

    Add files via upload

---
+++ Dec25Part2.js
@@ -0,0 +1,30 @@
+switch (day) {
+    case 'monday':
+        console.log('Code for an hour');
+        break;
+    case 'tuesday':
+        console.log('break day!');
+    case 'wendsday':
+        console.log('Code for an hour');
+    case 'thursday':
+        console.log('break day');
+    case 'friday':
+        console.log('Do 3 pages of kumon');
+    case 'saturday':
+    case 'sunday':
+        console.log('2 hours of coding and kumon AND grading :(');
+}
+
+if (day === 'monday') {
+    console.log('Code for an hour');
+} else if (day === 'tuesday'); {
+    console.log('break day!');
+} else if (day === 'wendsday') {
+    console.log('Code for an hour');
+} else if (day === 'thursday') {
+    console.log('break day!');
+} else (day === 'friday') {
+    console.log('Do 3 pages of kumon');
+} (day === 'saturday' || day === sunday) {
+    console.log('2 hours of coding and kumon AND grading :(');
+}
